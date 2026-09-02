f11fb4b8087731328cf9ca0362d56aaacc797151
ericj706 <ericj6215@gmail.com>
2026-09-02 15:51:56 +0900

    day04_16:00

--- src/main/java/example/day04/ExamController.java
+++ src/main/java/example/day04/ExamController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -33,4 +38,17 @@ public class ExamController {
         return examService.saveAll(entity);
     }
     
+    // [3] 삭제
+    @DeleteMapping("/day04/exam")
+    public boolean examDelete(@RequestParam(name = "no") int no){
+        // 서비스호출
+        return examService.examDelete(no);
+    }
+
+    // [4] 수정
+    @PutMapping("/day04/exam")
+    public boolean examUpdate( @RequestBody ExamEntity entity) {
+        // 서비스호출
+        return examService.examUpdate(entity);
+    }
 }
--- src/main/java/example/day04/ExamService.java
+++ src/main/java/example/day04/ExamService.java
@@ -1,9 +1,11 @@
 package example.day04;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 // Controller에서 받고 Repository로 전
@@ -32,4 +34,29 @@ public class ExamService {
         return false;
     }
 
+    // [3] 삭제
+    public boolean examDelete(int no){
+        // 리포지토리 호출
+        // 리포지토리객체.deleteById(삭제할 PK번호) : delete SQL 지원
+        // 리포지토리객체.delete(삭제할 엔티티)
+        // 반환타입: 없음, 삭제여부 findxxx 이용하여 확인
+        examRepository.deleteById(no);
+        return true;
+    }
+    
+    // [4] 수정
+    @Transactional
+    public boolean examUpdate(ExamEntity entity){
+        // 1. 영속된 엔티티 조회[PK: 수정할 번호]
+        // 리포지토리객체.findById(조회할pk번호): select SQL 지원
+        Optional<ExamEntity> optional = examRepository.findById(entity.getEno());
+        // 2. 조회된 결과 엔티티 여부 확인
+        // Optional클래스란? 본문(객체) 감싼 클래스 ( 왜? null 예외 안전하게 사용 )
+        if(optional.isPresent()){   // 객체가 있으면 true, 없으면 false
+            ExamEntity savedEntity = optional.get(); // 래핑된 Optional에서 엔티티 꺼내기
+            savedEntity.setEname(entity.getEname());
+            return true;
+        }                                                                                                                                                                                                                                                                  
+        return false;
+    }
 }
